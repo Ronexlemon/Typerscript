cea6c9b732d5a3f3d853c889765c44456dfe34ff
RonexLemon <ronexondimu@gmail.com>
2023-07-07 14:19:05 +0300

    Interface

---
+++ src/interface.ts
@@ -0,0 +1,10 @@
+interface dimensions{
+    length:number,
+    width:number
+}
+
+let reactange: dimensions={
+    length:4,
+    width:5
+}
+console.log("area is",reactange.length*reactange.width)
