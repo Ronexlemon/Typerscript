214dca8a6eba33b9c239ba9d84014c4278f62d59
RonexLemon <ronexondimu@gmail.com>
2023-07-07 14:14:58 +0300

    add enums

---
+++ dist/enumss.js
@@ -0,0 +1,11 @@
+"use strict";
+var Size;
+(function (Size) {
+    Size[Size["small"] = 0] = "small";
+    Size[Size["medium"] = 1] = "medium";
+    Size[Size["large"] = 2] = "large";
+    Size[Size["exl"] = 3] = "exl";
+})(Size || (Size = {}));
+console.log(Size.small);
+console.log(Size.medium);
+//# sourceMappingURL=enumss.js.map
